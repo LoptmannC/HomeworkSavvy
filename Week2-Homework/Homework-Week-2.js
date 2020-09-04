40e23d2f1434439026aafe09bfdce203875c6614
Coulson Loptmann <loptmann.c@gmail.com>
2020-09-03 19:55:58 -0500

    Corrected Bonus

--- Week2-Homework/Homework-Week-2.js
+++ Week2-Homework/Homework-Week-2.js
@@ -19,13 +19,10 @@ if (numberOfToppings < 10) {
 
 //Bonus Challenge
 //loop to print all even numbers from 1 to numberOfToppings (or 10)
-let base = 0;
 
 for (let base = 0; base <= 10; base += 2) {
   console.log(base);
 }
-
-let toppings = 5;
 
 for (let toppings = 0; toppings <= numberOfToppings; toppings += 2) {
   console.log(toppings);
